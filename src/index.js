fb7c2324a15b6764139ed130fa4657d910115f77
AlekseiMinsk <budanov.info@gmail.com>
2019-03-24 22:32:42 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+	let error  = {error: "You are rich, my friend! We don't have so much coins for exchange"}
+	
+	if(currency > 10000) {
+		return error;
+	}
+	if(currency === 0) {
+		return {};	
+	}
+	let answer = {};
+	let coinsArr = [50, 25, 10, 5, 1];
+	let namesArr = [0, 0, 0, 0, 0];
+	let letterArr = ["H", "Q", "D", "N", "P"];
+	coinsArr.forEach((item, i) =>{
+		while(currency >= item) {
+			currency = currency - item;
+			namesArr[i]++;
+		}
+	})
+	namesArr.forEach((item, i) =>{
+		if(item){
+			answer[letterArr[i]] = item;
+		}
+	})
+	return answer;
 }
